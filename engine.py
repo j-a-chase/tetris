d502e3c8e250070c8e0408e25ae3a63d30d4f284
James Chase <gochases@gmail.com>
2023-11-06 22:07:38 -0700

    Add engine.py

---
+++ engine.py
@@ -0,0 +1,22 @@
+######################################################################################################################################################
+# Name: James A. Chase
+# File: engine.py
+# Date: 6 November 2023
+# Description:
+#
+# Class file for Engine class. Handles pygame assets and graphical generation.
+#
+######################################################################################################################################################
+
+class Engine:
+
+    def __init__(self):
+        '''
+        Constructor
+
+        Parameters: None
+
+        Returns: None
+        '''
+
+if __name__ == '__main__': assert False, "This is a class file. Please import its contents into another file."
