8d27091bf16f33149fe63118ba1f341357e6574e
James Chase <gochases@gmail.com>
2023-11-06 22:22:09 -0700

    Add figure.py file

---
+++ figure.py
@@ -0,0 +1,25 @@
+######################################################################################################################################################
+# Name: James A. Chase
+# File: figure.py
+# Date: 6 November 2023
+# Description:
+#
+# Class file for the Figure class. Represents a Tetris piece (Tetrimino).
+#
+######################################################################################################################################################
+
+# imports
+
+
+class Figure:
+
+    def __init__(self) -> None:
+        '''
+        Constructor
+
+        Parameters: None
+
+        Returns: None
+        '''
+
+if __name__ == '__main__': assert False, "This is a class file. Please import its contents into another file."
